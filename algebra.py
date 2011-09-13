41942a0ebca19b802c66cf65c50fa004f158e6a2
Matt Hagy <hagy@gatech.edu>
2011-09-13 18:10:11 -0400

    Added comments/docs, introduced betters names, and minor formatting improvements

--- algebra.py
+++ algebra.py
@@ -10,7 +10,7 @@ try:
 except ImportError:
     numpy_number_type = None
 
-from jamenson.runtime.multimethod import MultiMethod, defmethod, defboth_wrapper, current_method
+from jamenson.runtime.multimethod import MultiMethod, defmethod, defboth_wrapper
 from jamenson.runtime.atypes import anytype, as_optimized_type
 from jamenson.runtime.func import identity
 
@@ -34,72 +34,82 @@ lt le eq ge gt ne
 
 __all__ += binop_names
 
-def make_mms():
+def construct_multimethods():
+    '''Create all of the algebric mutlimethods (e.g. mm_add)
+    '''
     gbls = globals()
+
     global mm_unop_base
     mm_unop_base = MultiMethod(name='mm_unop_base')
+
     for name in unop_names:
         mm_name = 'mm_' + name
         gbls[mm_name] = MultiMethod(name=mm_name,
-                                    #signature='op',
                                     doc='''multimethod for unary operation %s
                                     ''' % (name,),
                                     inherit_from=[mm_unop_base])
+
     global mm_binop_base
     mm_binop_base = MultiMethod(name='mm_binop_base')
+
     for name in binop_names:
         mm_name = 'mm_' + name
         gbls[mm_name] = MultiMethod(name=mm_name,
-                                    #signature='object,object',
                                     doc='''multimethod for binary operation %s
                                     ''' % (name,),
                                     inherit_from=[mm_binop_base])
         gbls['defboth_mm_' + name] = partial(defboth_wrapper, gbls[mm_name])
 
-make_mms()
-del make_mms
+construct_multimethods()
+del construct_multimethods
+
 
 class AlgebraBase(object):
+    '''Base class that translates Python algebric operations (e.g. __add__)
+       into multimethod calls (e.g. mm_add)
+    '''
 
-    def make_methods(locs=locals()):
+    def construct_methods(locs=locals()):
+
+        gbls = globals()
+
         def make_wrapper(name, func):
             meth_name = '__%s__' % name
             func.func_name = meth_name
             locs[meth_name] = func
+
         def make_unop_wrapper(name):
             mm = gbls['mm_%s' % name]
             make_wrapper(name, lambda op: mm(op))
+
         def make_binop_wrapper(name):
             mm = gbls['mm_%s' % name]
             make_wrapper(name, lambda lop, rop: mm(lop, rop))
+
         def make_binrop_wrapper(name):
             mm = gbls['mm_%s' % name]
             make_wrapper('r'+name, lambda rop, lop: mm(lop, rop))
-        gbls = globals()
+
         for name in unop_names:
             make_unop_wrapper(name)
+
         for name in binop_names:
             make_binop_wrapper(name)
             make_binrop_wrapper(name)
 
-    make_methods()
-    del make_methods
+    construct_methods()
+    del construct_methods
 
+
+# Default multimethod behavior
 
 @defmethod(mm_unop_base, [anytype])
 def meth(op):
     return NotImplemented
-#     raise TypeError("no method defined for %s wtih type %s" %
-#                     (current_method().name, type(op).__name__))
-
 
 @defmethod(mm_binop_base, [anytype, anytype])
 def meth(lop, rop):
     return NotImplemented
-#     raise TypeError("no method defined for %s with types (%s,%s)" %
-#                     (current_method().name,
-#                      type(lop).__name__,
-#                      type(rop).__name__))
 
 @defmethod(mm_eq, [AlgebraBase, AlgebraBase])
 def meth(a,b):
@@ -119,13 +129,12 @@ def meth(a,s):
 
 
 class DivAlgebraBase(AlgebraBase):
+    '''Base class where truediv is delegated out to div
+       (i.e. truediv is semantically equivalent to div)
+    '''
 
-    pass
+@defboth_wrapper(mm_truediv, [DivAlgebraBase, anytype])
+def meth(a, b):
+    return mm_div(a, b)
 
-@defmethod(mm_truediv, [DivAlgebraBase, anytype])
-def meth(dab, x):
-    return mm_div(dab, x)
 
-@defmethod(mm_truediv, [anytype, DivAlgebraBase])
-def meth(x, dab):
-    return mm_div(x, dab)
